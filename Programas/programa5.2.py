8945bc80805286ba3d9016931844cf5f1fd39c80
Alejandro Ribeiro Prieto <147511896+Prieto8835@users.noreply.github.com>
2024-06-27 11:13:50 -0300

    Add files via upload

---
+++ Programas/programa5.2.py
@@ -0,0 +1,9 @@
+#tabuada com repetições aninhadas
+tabuada = 1
+while tabuada <= 10:
+    número = 1
+    while número <= 10:
+        print(f'{tabuada} x {número} = {tabuada * número} ')
+        número += 1
+    tabuada += 1
+ 
